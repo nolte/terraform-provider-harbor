857f91ffbae58d8ad40f46f94d0ef3dc552a06dd
nolte <nolte07@googlemail.com>
2020-05-27 21:05:41 +0200

    fixing/ignore shell error

--- scripts/tst-15-execute-go-acc.sh
+++ scripts/tst-15-execute-go-acc.sh
@@ -8,8 +8,12 @@ set -o pipefail
 set -o nounset
 
 export TF_ACC=1
-export HARBOR_ENDPOINT="$(kubectl get Ingress tf-harbor-test-harbor-ingress -n harbor -ojson | jq '.spec.rules[].host' -r | grep harbor)"
+HARBOR_ENDPOINT="$(kubectl get Ingress tf-harbor-test-harbor-ingress -n harbor -ojson | jq '.spec.rules[].host' -r | grep harbor)"
+export HARBOR_ENDPOINT
 export HARBOR_USERNAME=admin
-export HARBOR_PASSWORD="$(kubectl -n harbor get secret tf-harbor-test-harbor-core -ojson | jq '.data.HARBOR_ADMIN_PASSWORD' -r | base64 -d)"
+HARBOR_PASSWORD="$(kubectl -n harbor get secret tf-harbor-test-harbor-core -ojson | jq '.data.HARBOR_ADMIN_PASSWORD' -r | base64 -d)"
+export HARBOR_PASSWORD
 export HARBOR_INSECURE="true"
+
+# shellcheck disable=SC2046
 go test -timeout 20m $(go list /go/src/github.com/nolte/terraform-provider-harbor/... |grep -v 'vendor') -v
