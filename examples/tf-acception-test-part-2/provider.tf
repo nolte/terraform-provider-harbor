aafc4a471b555e6cf4f1409b375a9d52ce0f6607
nolte <nolte07@googlemail.com>
2020-07-26 13:57:28 +0200

    remove unused comment

--- examples/tf-acception-test-part-2/provider.tf
+++ examples/tf-acception-test-part-2/provider.tf
@@ -1,8 +1,4 @@
+# will be configure by env vars from script
 provider "harbor" {
-  #host     = "harbor.172-17-0-1.sslip.io"
-  #schema   = "https"
-  #insecure = true
-  #basepath = "/api/v2.0"
-  #username = "admin"
-  #password = "Harbor12345"
+
 }
--- examples/tf-project-only/provider.tf
+++ examples/tf-project-only/provider.tf
@@ -1,12 +1,4 @@
-
-
-
-# example for harbor v1 api usage
+# will be configure by env vars from script
 provider "harbor" {
-  #host     = "harbor.172-17-0-2.sslip.io"
-  #schema   = "https"
-  #insecure = true
-  #basepath = "/api/v2.0"
-  #username = "admin"
-  #password = "Harbor12345"
+
 }
